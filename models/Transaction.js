b5a072e7d2c8f91500096c8f99bb626a028b5e71
Dimitra Prousalis <dimitra.o.p@gmail.com>
2023-04-25 12:04:13 -0700

    first commit

---
+++ models/Transaction.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const TransactionSchema = new mongoose.Schema({
+  transaction: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Transaction', TransactionSchema)
