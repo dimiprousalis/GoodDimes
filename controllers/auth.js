eb60b97c06fdfff165586ca8f491482fa2105516
Dimitra Prousalis <dimitra.o.p@gmail.com>
2023-04-25 20:29:35 -0700

    fix logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,13 +36,9 @@ const User = require('../models/User')
     })(req, res, next)
   }
   
-  exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
+  exports.logout = (req, res, next) => {
+    req.logout((error) => {
+      if (error) { return next(error) }
       res.redirect('/')
     })
   }
