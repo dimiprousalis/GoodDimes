d055b4c3b0aa97653c0f764dac50b77e282a35c0
Dimitra Prousalis <dimitra.o.p@gmail.com>
2023-05-02 19:48:16 -0700

    clean up getTransactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,47 +1,64 @@
 const Transaction = require('../models/Transaction')
 
 module.exports = {
-    getTransactions: async (req,res)=>{
-        try{
-            const transactionItems = await Transaction.find({userId:req.user.id});
-            //Calculate the dollar splits between how much user/friend consumed and how much they spent
-            const totalAmount = transactionItems.reduce((a, obj) => a + obj.amount, 0);
-            const totalUserAmount = transactionItems.reduce((a, obj) => a + obj.userPortion, 0);
-            const totalFriendAmount = transactionItems.reduce((a, obj) => a + obj.friendPortion, 0);
-            const totalUserSpent = transactionItems.filter(obj => obj.payer === "user").reduce((a, obj) => a + obj.amount, 0);
-            const totalFriendSpent = totalAmount - totalUserSpent;
+    getTransactions: async (req, res) => {
+        try {
+            const [transactions, sums] = await Promise.all([
+                Transaction.find({ userId: req.user.id }),
+                Transaction.aggregate([
+                    { $match: { userId: req.user.id } },
+                    {
+                        $group: {
+                            _id: null,
+                            totalAmount: { $sum: "$amount" },
+                            totalUserAmount: { $sum: "$userPortion" },
+                            totalFriendAmount: { $sum: "$friendPortion" },
+                            totalUserSpent: {
+                                $sum: {
+                                    $cond: [{ $eq: ["$payer", "user"] }, "$amount", 0]
+                                }
+                            }
+                        }
+                    }
+                ])
+            ]);
+
+            const { totalAmount, totalUserAmount, totalFriendAmount, totalUserSpent } = sums[0];
 
             res.render('transactions.ejs', {
-                transactions: transactionItems, 
-                user: req.user, 
-                amountSum: totalAmount, 
+                transactions,
+                user: req.user,
+                amountSum: totalAmount,
                 userAmountSum: totalUserAmount,
                 friendAmountSum: totalFriendAmount,
                 userSpentSum: totalUserSpent,
-                friendSpentSum: totalFriendSpent})
-        }catch(err){
+                friendSpentSum: totalAmount - totalUserSpent
+            })
+        } catch (err) {
             console.log(err)
         }
     },
-    createTransaction: async (req, res)=>{
-        try{
+
+    createTransaction: async (req, res) => {
+        try {
             // Calculates transaction split amount
-            const userAmt = req.body.splitItem === "equally"? req.body.amountItem/2 : req.body.userAmountItem;
-            const friendAmt = req.body.splitItem === "equally"? req.body.amountItem/2 : req.body.amountItem - req.body.userAmountItem;
+            const userAmt = req.body.splitItem === "equally" ? req.body.amountItem / 2 : req.body.userAmountItem;
+            const friendAmt = req.body.splitItem === "equally" ? req.body.amountItem / 2 : req.body.amountItem - req.body.userAmountItem;
             // Creates a new transaction record in the database
-            await Transaction.create({date: req.body.dateItem, transaction: req.body.transactionItem, amount: req.body.amountItem, payer: req.body.payerItem, userPortion: userAmt, friendPortion: friendAmt, userId: req.user.id})
+            await Transaction.create({ date: req.body.dateItem, transaction: req.body.transactionItem, amount: req.body.amountItem, payer: req.body.payerItem, userPortion: userAmt, friendPortion: friendAmt, userId: req.user.id })
             console.log('Transaction has been added!')
             res.redirect('/transactions')
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     },
-    deleteTransaction: async (req, res)=>{
-        try{
-            await Transaction.findOneAndDelete({_id:req.body.transactionIdFromJSFile})
+    
+    deleteTransaction: async (req, res) => {
+        try {
+            await Transaction.findOneAndDelete({ _id: req.body.transactionIdFromJSFile })
             console.log('Deleted Transaction')
             res.json('Deleted It')
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
